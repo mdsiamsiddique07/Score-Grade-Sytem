d15b5dc114938e880b3c21176dfedce1091680e7
mdsiamsiddique07 <mdsiamsiqqique777@gmail.com>
2024-04-07 15:54:09 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// for(let i =0;i<=10;i++){
+//     console.log(i)
+// }
+let marks = prompt ("PLESE ENTER YOUR MAERKS:")
+if (marks<33){
+    console.log("you are filed!!!!!!!!")
+} 
+else if(marks >=33  && marks <=40){
+console.log("YOU got an D")
+}
+else if(marks >=41  && marks <=50){
+    console.log("YOU got an c")
+    }
+    else if(marks >=51  && marks <=60){
+        console.log("YOU got an B")
+        }
+        else if(marks >=61  && marks <=70){
+            console.log("YOU got an A")
+            }
+            else{console.log("you got A+")}
